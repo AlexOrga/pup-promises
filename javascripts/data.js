65bca7c0def84a9f906db3ae51360408361d6ac8
Alex Orga <alexorga.dev@gmail.com>
2018-05-15 18:58:56 -0500

    updated code from lauren

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,17 +1,20 @@
-const domString = require('./dom');
+const dom = require('./dom');
 
+/* ALL PUP DATA FUNCTIONS */
+
+/* How we would have gotten all the data with JQuery AJAX △ */
 // const getAllPups = () => {
 //   let pups = [];
 //   $.get('../db/pup1.json')
 //     .done((data1) => {
-//       pups = data1.pup1;
+//       pups = [...data1.pup1,];
 //       $.get('../db/pup2.json')
 //         .done((data2) => {
 //           pups = [...pups, ...data2.pup2,];
 //           $.get('../db/pup3.json')
 //             .done((data3) => {
 //               pups = [...pups, ...data3.pup3,];
-//               domString(pups);
+//               dom.domString(pups);
 //             })
 //             .fail((error3) => {
 //               console.error(error3);
@@ -26,6 +29,7 @@ const domString = require('./dom');
 //     });
 // };
 
+/* Promise Constructors */
 const firstPupJSON = () => {
   return new Promise((resolve, reject) => {
     $.get('../db/pup1.json')
@@ -33,7 +37,7 @@ const firstPupJSON = () => {
         resolve(data.pup1);
       })
       .fail((err) => {
-        reject(`i got an error`, err);
+        reject(`Oi got an error!`, err);
       });
   });
 };
@@ -45,7 +49,7 @@ const secondPupJSON = () => {
         resolve(data.pup2);
       })
       .fail((err) => {
-        reject(`i got an error`, err);
+        reject(`Oi got an error!`, err);
       });
   });
 };
@@ -57,47 +61,29 @@ const thirdPupJSON = () => {
         resolve(data.pup3);
       })
       .fail((err) => {
-        reject(`i got an error`, err);
+        reject(`Oi got an error!`, err);
       });
   });
 };
 
-const firstFoodJSON = () => {
-  return new Promise((resolve, reject) => {
-    $.get('../db/food1.json')
-      .done((data) => {
-        resolve(data.food1);
-      })
-      .fail((err) => {
-        reject(`i got an error`, err);
-      });
-  });
-};
+/* Promise Pyramid of DOOM △ - works but is still confusing */
+// const getAllPups = () => {
+//   let pups = [];
+//   firstPupJSON().then((results) => {
+//     pups = [...results,];
+//     secondPupJSON().then((results2) => {
+//       pups = [...pups, ...results2,];
+//       thirdPupJSON().then((results3) => {
+//         pups = [...pups, ...results3,];
+//         printToDom(pups);
+//       }).catch((error) => {
+//         console.error('error', error);
+//       });
+//     });
+//   });
+// };
 
-const secondFoodJSON = () => {
-  return new Promise((resolve, reject) => {
-    $.get('../db/food2.json')
-      .done((data) => {
-        resolve(data.food2);
-      })
-      .fail((err) => {
-        reject(`i got an error`, err);
-      });
-  });
-};
-
-const thirdFoodJSON = () => {
-  return new Promise((resolve, reject) => {
-    $.get('../db/food3.json')
-      .done((data) => {
-        resolve(data.food3);
-      })
-      .fail((err) => {
-        reject(`i got an error`, err);
-      });
-  });
-};
-
+/* Promise Chaining Solution, returning a resolved promise with the data */
 // const getAllPups = () => {
 //   let dogos = [];
 //   return firstPupJSON()
@@ -110,37 +96,97 @@ const thirdFoodJSON = () => {
 //     }).then((result3) => {
 //       dogos = [...dogos, ...result3,];
 //       return Promise.resolve(dogos);
-//     })
-//     .catch((errorMsg) => {
+//     }).catch((errorMsg) => {
 //       console.error(errorMsg);
 //     });
 // };
 
+/*
+Promise.all Solution, returning a resolved promise with the data
+Best solution for this problem, because we aim to aggregate the data together quickly
+*/
 const getAllPups = () => {
   return Promise.all([firstPupJSON(), secondPupJSON(), thirdPupJSON(),])
     .then((results) => {
       const dogos = [...results[0], ...results[1], ...results[2],];
       return Promise.resolve(dogos);
-    })
-    .catch((error) => {
+    }).catch((error) => {
       console.error(error);
     });
 };
 
 const initializer = () => {
+  /* We just called this function at first before we started returning the resolved Promise */
+  // getAllPups();
   getAllPups().then((dogos) => {
-    domString(dogos);
+    dom.domString(dogos);
   });
-  singlePup();
 };
 
+/* SINGLE PUP DATA FUNCTIONS */
+
+/* Promise Constructors */
+const firstFoodJSON = () => {
+  return new Promise((resolve, reject) => {
+    $.get('../db/food1.json')
+      .done((data) => {
+        const foodArray = data.food1;
+        foodArray.map(food => food.key = 1);
+        resolve(foodArray);
+      })
+      .fail((err) => {
+        reject(`Oi got an error!`, err);
+      });
+  });
+};
+
+const secondFoodJSON = () => {
+  return new Promise((resolve, reject) => {
+    $.get('../db/food2.json')
+      .done((data) => {
+        const foodArray = data.food2;
+        foodArray.map(food => food.key = 2);
+        resolve(foodArray);
+      })
+      .fail((err) => {
+        reject(`Oi got an error!`, err);
+      });
+  });
+};
+
+const thirdFoodJSON = () => {
+  return new Promise((resolve, reject) => {
+    $.get('../db/food3.json')
+      .done((data) => {
+        const foodArray = data.food3;
+        foodArray.map(food => food.key = 3);
+        resolve(foodArray);
+      })
+      .fail((err) => {
+        reject(`Oi got an error!`, err);
+      });
+  });
+};
+
+/*
+Using PROMISE.ALL to get the matching foods for the first Pup
+Note: we are exporting this function
+*/
 const singlePup = () => {
   let pup = {};
-  getAllPups().then((pups) => {
-    pup = pups[0].favFoodId;
+  return getAllPups().then((pups) => {
+    pup = pups[0];
     return Promise.all([firstFoodJSON(), secondFoodJSON(), thirdFoodJSON(),]);
-  }).then((foods) => {
-    pup.favFoods = foods;
+  }).then((foodz) => {
+    const allTheFood = [...foodz[0], ...foodz[1], ...foodz[2],];
+    const matching = allTheFood.filter((food) => {
+      if (pup.favFoodId === food.key) {
+        return true;
+      };
+      return false;
+    });
+    pup.favFood = matching;
+    return Promise.resolve(pup);
   });
 };
 
